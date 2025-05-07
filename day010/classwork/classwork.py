1eb61f8af2d2f98c5685a14019191dc8378976e8
Lukachakhvashvili111 <lukachakhvashvili1@gmail.com>
2025-05-07 21:45:32 +0400

    commit

---
+++ day010/classwork/classwork.py
@@ -0,0 +1,20 @@
+
+names = ["Ana", "Luka", "Nino"]
+
+names.append("Banana")
+names.append("Apple")
+names.append("ფორთოხალი")
+
+print("Updated list (Part 1):", names)
+
+
+
+names_list = ["Ana", "Luka", "Nino", "Saba", "Tina", "Giorgi", "Mariam"]
+
+
+names_list.insert(3, "mtvare")
+
+
+names_list.insert(5, 65)
+
+print("Updated list (Part 2):", names_list)
