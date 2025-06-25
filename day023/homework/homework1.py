88417baf78cb40fdbba019b29547870dcba58a86
Lukachakhvashvili111 <lukachakhvashvili1@gmail.com>
2025-06-25 21:27:30 +0400

    m

---
+++ day023/homework/homework1.py
@@ -0,0 +1,13 @@
+#append ამატებს სადაც ვეტყვით
+#pop შლის სადაც ვეტყვით
+#remove შლის სახელით
+#insert შეაქვს სადაც ვეტყვით
+#extend აერთიანებს ორ სიას
+list = [1,2,42,2,123,]
+list.append(3333)
+list.insert(3,111)
+list.pop(4)
+list.remove(2)
+list2 = [333,222,111]
+list.extend(list2)
+print(list)
---
+++ day023/homework/homework3.py
@@ -0,0 +1,11 @@
+name = "luka"
+x = 0
+
+while x > len(name):
+    print(name[x])
+    x += 1
+
+
+
+for i in name():
+    print(i)
